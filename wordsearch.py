1d84e33e6d242e50d4df5ab65775bc23bcb3cf1a
dk2462000 <43301847+dk2462000@users.noreply.github.com>
2020-02-28 07:28:51 +0530

    final commit

--- wordsearch.py
+++ wordsearch.py
@@ -1,9 +1,28 @@
 import json
-data=data.load("076 data.json")
+from difflib import get_close_matches
+data=json.load("076 data.json")
 
-def translate(w)
+def translate(w):
     w=w.lower()
     if w in data:
         return data[w]
+    elif len(get_close_matches(w,data.keys()))>0:
+            ys=input("did you mean %s instead of enter y if prediction is correct else N") % get_close_matches(w,data.keys())[0]
+            if ys=="y":           
+                return data[w]
+            
+            elif ys=="n":
+                print("we couldn't understand the word")
+            
+            else:
+                print("there is no such word")
     else:
-        
+        print("there is no such word")                
+
+word=input("enter word")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
